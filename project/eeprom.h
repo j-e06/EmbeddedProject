7a677423eb6073d0249fa2763a204d0838a68c59
asdfg6 <patrik.peve@gmail.com>
2025-05-04 20:32:40 +0300

    eeprom maybe fixed

--- project/eeprom.h
+++ project/eeprom.h
@@ -16,6 +16,9 @@ bool save_state_to_eeprom(i2c_inst_t *i2c);
 bool load_state_from_eeprom(i2c_inst_t *i2c);
 bool check_need_recovery(void);
 void load_eeprom_state(i2c_inst_t *eeprom_i2c, repeating_timer_callback_t pill_timer_callback);
+void reset_calibration_values(i2c_inst_t *i2c);
+void reset_pill_count(i2c_inst_t *i2c);  // New function to reset only pill count
+
 // EEPROM low-level read/write
 bool eeprom_write_bytes(i2c_inst_t *i2c, uint16_t addr, const uint8_t *data, size_t len);
 bool eeprom_read_bytes(i2c_inst_t *i2c, uint16_t addr, uint8_t *data, size_t len);
